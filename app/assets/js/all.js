02dc65fa01379634c188f0d78501402cb962fc55
RayChen1996 <53119155+RayChen1996@users.noreply.github.com>
2022-11-28 21:56:09 +0800

    fix Bug

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,49 +1,65 @@
 
 $(function(){
-  GetProductList()
-  ShowMyCart()
+  getProductList()
+  showMyCart()
 })
+const list_Product = document.querySelector('.productWrap')
+const cartTbl = document.querySelector('.shoppingCart-table')
+const handleForm = document.querySelector(".orderInfo-form")
+const categorySelectChange = document.querySelector(".productSelect")
+
+const transactionType = document.querySelector('#tradeWay');
 
 let PData = []
-let CartArr = []
-function GetProductList(){
-  //https://livejs-api.hexschool.io/api/livejs/v1/customer/getproductlist/carts
-  const url = `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/products`
-  // console.log(`GetProductList  ${api_path}`)
- 
+let cartArr = []
+
+const constraints = {
+  "name": {
+      presence: {
+          message: "此欄位必填!",
+      }
+  },
+  "tel": {
+      presence: {
+          message: "此欄位必填!"
+      },
+  },
+  "email": {
+      presence: {
+          message: "此欄位必填!"
+      },
+      email: true
+  },
+  "address": {
+      presence: {
+          message: "此欄位必填!"
+      },
+  },
+}
+
+function getProductList(){
+  const url = `${BaseUrl}/api/livejs/v1/customer/${api_path}/products` 
   axios.get(url)
   .then(function (response) {
-    // 成功會回傳的內容
-    // console.log(response);
     let data = response.data
     PData = data.products
     if(data.status)
-       RenderProductData(data.products)
+       renderProductData(data.products)
   })
   .catch(function (error) {
     // 失敗會回傳的內容
     console.log(error);
   })
 
-
-
 }
 
 
-
-
-
-function ShowMyCart(){
-  // https://livejs-api.hexschool.io/api/livejs/v1/customer/raychen/carts
-  const url = `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts`
+function showMyCart(){
+  const url = `${BaseUrl}/api/livejs/v1/customer/${api_path}/carts`
   axios.get(url)
   .then(function (response) {
     // 成功會回傳的內容
-    console.log('我的購物車');
-    console.log(response);
-    ReRenderMyCart(response.data)
-
-  
+    reRenderMyCart(response.data)
   })
   .catch(function (error) {
     // 失敗會回傳的內容
@@ -51,22 +67,29 @@ function ShowMyCart(){
   })  
 }
 
-function AddMyCart(PId){
-  // console.log(`PId  ${PId}`)
-  axios.post(`https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts`, {
+function addMyCart(pId){
+  let buyCount = 0
+  cartArr.forEach((item) => {
+    if (item.product.id == pId) {
+      item.quantity+=1; 
+      buyCount = item.quantity;
+    }
+  });
+ 
+  axios.post(`${BaseUrl}/api/livejs/v1/customer/${api_path}/carts`, {
     //帶入的值會使用物件包裝
     "data": {
-      "productId": PId,
-      "quantity": 1
+      "productId": pId,
+      "quantity": buyCount
     }
   })
   .then(function (response) {
     // 成功會回傳的內容
+ 
     let data = response.data
-    CartArr = data.carts
-    console.log(response)
+    
     if(data.status){
-      ReRenderMyCart(data)
+      reRenderMyCart(data)
       Swal.fire({
         title: '新增成功!',
         text: '',
@@ -89,8 +112,8 @@ function AddMyCart(PId){
 }
 
 //更新我的購物車列表
-function ReRenderMyCart(PDArr){
-  const CartTbl = document.querySelector('.shoppingCart-table')
+function reRenderMyCart(PDArr){
+  cartArr = PDArr.carts
   //表頭
   const myCartHTML_TH= `
       <tr>
@@ -102,20 +125,13 @@ function ReRenderMyCart(PDArr){
       </tr>
 
   `
-  
   let myCartHTML = ``
-  let TotalPrice = PDArr.finalTotal
+  let totalPrice = PDArr.finalTotal
   if(PDArr.carts.length==0){
     myCartHTML += ShowEmptyTbl();
-    Swal.fire({
-      title: '操作成功!',
-      text: '購物車已清空！！',
-      icon: 'success',
-      confirmButtonText: 'OK 哩'
-    })
   }else{
     PDArr.carts.forEach((pItemObj,pIdx)=>{
-      
+     
       myCartHTML += `
         <tr>
           <td>
@@ -124,11 +140,11 @@ function ReRenderMyCart(PDArr){
                   <p>${pItemObj.product.title}</p>
               </div>
           </td>
-          <td>NT$${pItemObj.product.origin_price}</td>
-          <td>1</td>
           <td>NT$${pItemObj.product.price}</td>
+          <td>${pItemObj.quantity}</td>
+          <td>NT$${pItemObj.product.price*pItemObj.quantity}</td>
           <td class="discardBtn">
-              <a onclick=DelSingleCart("${pItemObj.id}") href="#javascript:;" class="material-icons">
+              <a onclick=delSingleCart("${pItemObj.id}") href="#javascript:;" class="material-icons">
                   clear
               </a>
           </td>
@@ -143,31 +159,26 @@ function ReRenderMyCart(PDArr){
   const myCartHTML_Tfoot= `
     <tr>
       <td>
-          <a href="#javascript:;" onclick='DelAllCart();' class="discardAllBtn">刪除所有品項</a>
+          <a href="#javascript:;" onclick='delAllCart();' class="discardAllBtn">刪除所有品項</a>
       </td>
       <td colspan="2"></td>
 
       <td>
           <p>總金額</p>
       </td>
-      <td>NT$${TotalPrice}</td>
+      <td>NT$${totalPrice}</td>
     </tr>
 
   `
-  CartTbl.innerHTML = myCartHTML_TH + myCartHTML + myCartHTML_Tfoot
+  cartTbl.innerHTML = myCartHTML_TH + myCartHTML + myCartHTML_Tfoot
  
-
 }
 
 
 // 刪除購物車內特定產品
-function DelSingleCart(pid){
-  //'https://livejs-api.hexschool.io/api/livejs/v1/customer/raychen/carts/jw1jNknjVsKYPNyqYSWA'
-  // console.log(`delete at  ${pid}`)
-  //https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts/jw1jNknjVsKYPNyqYSWA
-  axios.delete(`https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts/${pid}`).
+function delSingleCart(pid){
+  axios.delete(`${BaseUrl}/api/livejs/v1/customer/${api_path}/carts/${pid}`).
     then(function (response) {
-      console.log(response.data);
       if(response.data.status){
         Swal.fire({
           title: '刪除成功!',
@@ -183,53 +194,70 @@ function DelSingleCart(pid){
           confirmButtonText: 'OK'
         })
       }
-      ReRenderMyCart(response.data);
+      reRenderMyCart(response.data);
   })
-
-
 }
 
 //清除購物車內全部產品
-function DelAllCart(){
-  //https://livejs-api.hexschool.io/api/livejs/v1/customer/raychen/carts
-  axios.delete(`https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts`).
+function delAllCart(){
+  axios.delete(`${BaseUrl}/api/livejs/v1/customer/${api_path}/carts`).
     then(function (response) {
-      console.log(response.data);
       //重新渲染一次List  carts
-
-      ReRenderMyCart(response.data);
-      
-      
-  })
-
-}
-
-function AddPresertData(){
-  // console.log($("#tradeWay").val())
-  //https://livejs-api.hexschool.io/api/livejs/v1/customer/raychen/orders
-  axios.post(`https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/orders`, {
-    //帶入的值會使用物件包裝
-    "data": {
-      "user": {
-        "name": document.getElementById("customerName"),
-        "tel": document.getElementById("customerPhone"),
-        "email": document.getElementById("customerEmail"),
-        "address": document.getElementById("customerAddress"),
-        "payment": $("#tradeWay").val()
-      }
-    }
-  })
-  .then(function (response) {
-    // 成功會回傳的內容
-    let data = response.data
-    console.log(response)
-
+      reRenderMyCart(response.data);
   })
   .catch(function (error) {
     // 失敗會回傳的內容
     console.log(error);
   });
 
+}
+
+function addPresertData(){
+  let currentOpt = transactionType.options[transactionType.selectedIndex]; 
+  const error = 
+     validate(
+      {
+        email: document.getElementById("customerEmail").value,
+        name:document.getElementById("customerName").value,
+        address:document.getElementById("customerEmail").value,
+        tel:document.getElementById("customerPhone").value
+      }, 
+      constraints);
+
+  if(error==undefined){
+    //表示驗證通過
+    axios.post(`${BaseUrl}/api/livejs/v1/customer/${api_path}/orders`, {
+      //帶入的值會使用物件包裝
+      "data": {
+        "user": {
+          "name": document.getElementById("customerName").value,
+          "tel": document.getElementById("customerPhone").value,
+          "email": document.getElementById("customerEmail").value,
+          "address": document.getElementById("customerAddress").value,
+          "payment": currentOpt.value 
+        }
+      }
+    })
+    .then(function (response) {
+      // 成功會回傳的內容
+      let data = response.data
+
+
+    })
+    .catch(function (error) {
+      // 失敗會回傳的內容
+      console.log(error);
+    });
+
+  }else{
+
+    
+
+
+
+  }
+
+
 
 }
 
@@ -237,30 +265,22 @@ function AddPresertData(){
 
 
 
-let CaegorySelectChange = document.querySelector(".productSelect")
-CaegorySelectChange.addEventListener("change",(e)=>{
-  console.log(e.target.value)
+
+categorySelectChange.addEventListener("change",(e)=>{
   if(e.target.value=="全部"){
-    RenderProductData(PData)
+    renderProductData(PData)
   }else{
-    RenderProductData(FilterProduct(e.target.value))
+    renderProductData(FilterProduct(e.target.value))
   }
 })
 
-
-// let SubmitPresertOrrder = document.querySelector(".orderInfo-btn")
-// SubmitPresertOrrder.addEventListener("click",()=>{
-//   AddPresertData()
-// })
+ 
 
 
-let handleForm = document.querySelector(".orderInfo-form")
 
 handleForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  AddPresertData();
-
-  
+  addPresertData();
 });
 
 
@@ -272,27 +292,43 @@ function FilterProduct(qCategory){
   })
 }
 
-function RenderProductData(ProductArr){
-  console.log(ProductArr)
-  var HTMLStr = ""
-  let list_Product = document.querySelector('.productWrap')
-  ProductArr.forEach((item,index)=>{
+function renderProductData(productArr){
+  let HTMLStr = ""
+  
+  productArr.forEach((item,index)=>{
     HTMLStr += `
     <li class="productCard">
                 <h4 class="productType">${item.category}</h4>
                 <img src="${item.images}" alt="">
-                <a href="#javascript:;"  class="addCardBtn" onclick=AddMyCart("${item.id}") data-pid="${item.id}" >加入購物車</a>
+                <a href="#javascript:;"  class="addCardBtn" onclick=addMyCart("${item.id}") data-pid="${item.id}" >加入購物車</a>
                 <h3>${item.title}</h3>
                 <del class="originPrice">NT${item.origin_price}</del>
                 <p class="nowPrice">NT${item.price}</p>
             </li>
     `
   })
-  list_Product.innerHTML=HTMLStr
+  list_Product.innerHTML = HTMLStr
 }
 
 
 
+
+
+//將SweetAlert封裝
+function ShowToast(title,text,isSuccess,confirmButton){
+  let iconMsg = ''
+  if (isSuccess){
+    iconMsg = 'success'
+  }else{
+    iconMsg = 'error'
+  }
+  Swal.fire({
+    title: title,
+    text: text,
+    icon: iconMsg,
+    confirmButtonText: confirmButton
+  })
+}
 
 //秀空白td
 function ShowEmptyTbl(){
--- app/assets/js/config.js
+++ app/assets/js/config.js
@@ -1,2 +1,3 @@
 const api_path = 'raychen'
 const token = "zjRahQTiUTdZHg18y4XB5gv1Ort2"
+const BaseUrl = "https://livejs-api.hexschool.io"
